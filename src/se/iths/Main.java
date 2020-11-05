a3641dd27f24122b06d873553bfa8c675338876e
Sofia Sinani <sofsin1208@gmail.com>
2020-11-05 10:51:34 +0100

    Second commit

--- src/se/iths/Main.java
+++ src/se/iths/Main.java
@@ -2,6 +2,6 @@ package se.iths;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First version");
+        System.out.println("Second version");
     }
 }
